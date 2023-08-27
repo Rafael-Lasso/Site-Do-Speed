8a2557cc2d0b476ecc17c1025115d898000c8412
Rafael Lasso <rafael.comercial27@gmail.com>
2023-08-27 12:17:14 -0300

    spring boot project created

---
+++ springboot/src/test/java/com/SpeedUzumaki/SpeedOtakuCentral/SpeedOtakuCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpeedUzumaki.SpeedOtakuCentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedOtakuCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
