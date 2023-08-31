1f58617459c20d3a5cb434cfccbd42c45574e607
Rafael Lasso <rafael.comercial27@gmail.com>
2023-08-30 22:49:31 -0300

    project deleted due to problem with security and authentication dependency, new project already in operation but without security and authentication dependency

--- spring-boot/src/main/java/com/speed/coment/CommentaryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.speed.coment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentaryRepository extends JpaRepository<Commentary, Long>{
-
-}
