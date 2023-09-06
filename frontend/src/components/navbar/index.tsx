046f7f417422f448e54e3570ee26c94bbce9f81e
Rafael Lasso <rafael.comercial27@gmail.com>
2023-09-06 14:42:58 -0300

    Transition from pure CSS to Tailwind CSS framework started

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,36 +1,38 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import LogoSite from "./LogoSite.png";
+import LogoSite from "./logoSite.png";
 import "./style.css";
 
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar">
-        <div className="menu">
-          <div className="line1"></div>
-          <div className="line2"></div>
-          <div className="line3"></div>
+      <nav className=" place-items-center justify-evenly h-32 border-t-4 items-center flex bg-zinc-900 border-orange-500 text-zinc-100">
+        <div className="p-1 cursor-pointer">
+          <div className="m-1.5 w-10 h-1 rounded bg-slate-100"></div>
+          <div className="m-1.5 w-5 h-1 rounded bg-slate-100"></div>
+          <div className="m-1.5 w-8 h-1 rounded bg-slate-100"></div>
         </div>
-        <Image
-          className="LogoSiteImage"
-          src={LogoSite}
-          width={250}
-          height={250}
-          alt="Logo Speed Otaku Central"
-        />
-        <ul>
-          <li>
+        <Link href="/">
+          <Image
+            className="p-1 cursor-pointer"
+            src={LogoSite}
+            width={250}
+            height={250}
+            alt="Logo Speed Otaku Central"
+          />
+        </Link>
+        <ul className="flex gap-2 text-zinc-100">
+          <li className="cursor-pointer p-1 mt-3 mb-3 relative">
             <Link href="/">Início</Link>
           </li>
-          <li>
+          <li className="cursor-pointer p-1 mt-3 mb-3 relative">
             <Link href="/Curiosidades">Curiosidades</Link>
           </li>
-          <li>
-            <Link href="/Noticias">Noticias</Link>
+          <li className="cursor-pointer p-1 mt-3 mb-3 relative">
+            <Link href="/Notícias">Notícias</Link>
           </li>
-          <li>
+          <li className="cursor-pointer p-1 mt-3 mb-3 relative">
             <Link href="/Mangás">Mangás</Link>
           </li>
         </ul>
