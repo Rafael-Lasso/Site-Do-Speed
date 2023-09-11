2d2bbe409c3539051d92223a48f74571c863bb0b
Rafael Lasso <rafael.comercial27@gmail.com>
2023-09-11 14:36:58 -0300

    created golang project

---
+++ golang-notification/go.mod
@@ -0,0 +1,5 @@
+module github.com/rafael-lasso/golang-notification
+
+go 1.19
+
+require github.com/google/uuid v1.3.1 // indirect
---
+++ golang-notification/internal/domain/entities/notification.go
@@ -0,0 +1,10 @@
+package entities
+
+import "github.com/google/uuid"
+
+type Notification struct {
+	Id      uuid.UUID
+	Title   string
+	Message string
+	DateMsg string
+}
