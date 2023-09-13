e074e44aa4ba9e8bdaffce757fce465adff2650e
Rafael Lasso <rafael.comercial27@gmail.com>
2023-09-13 20:35:36 -0300

    Message service via WhatsApp working, but only when the env file is in the directory

--- golang-notification/go.mod
+++ golang-notification/go.mod
@@ -5,10 +5,21 @@ go 1.19
 require github.com/google/uuid v1.3.1
 
 require (
+	github.com/Rhymen/go-whatsapp v0.1.1 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/mock v1.6.0 // indirect
+	github.com/golang/protobuf v1.3.0 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
+	github.com/gorilla/websocket v1.4.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sendgrid/rest v2.6.9+incompatible // indirect
 	github.com/sendgrid/sendgrid-go v3.13.0+incompatible // indirect
+	github.com/sfreiberg/gotwilio v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/twilio/twilio-go v1.13.0 // indirect
+	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/tools v0.13.0 // indirect
 )
--- golang-notification/internal/domain/messages/whatsapp/whatsapp.go
+++ golang-notification/internal/domain/messages/whatsapp/whatsapp.go
@@ -3,7 +3,10 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/twilio/twilio-go"
 	api "github.com/twilio/twilio-go/rest/api/v2010"
 )
@@ -11,11 +14,22 @@ import (
 func main() {
 	// Find your Account SID and Auth Token at twilio.com/console
 	// and set the environment variables. See http://twil.io/secure
-	client := twilio.NewRestClient()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	accountSid := os.Getenv("ACCOUNT_SID")
+	authToken := os.Getenv("AUTH_TOKEN")
+
+	client := twilio.NewRestClientWithParams(twilio.ClientParams{
+		Username: accountSid,
+		Password: authToken,
+	})
 
 	params := &api.CreateMessageParams{}
 	params.SetFrom("whatsapp:+14155238886")
-	params.SetBody("Hello there!")
+	params.SetBody("Acabou de sair um Post novo com um tema que voce vai adorar! Vem ver 😁:")
 	params.SetTo("whatsapp:+5518997635588")
 
 	resp, err := client.Api.CreateMessage(params)
@@ -24,8 +38,11 @@ func main() {
 	} else {
 		if resp.Sid != nil {
 			fmt.Println(*resp.Sid)
+			fmt.Println("Message sent successfully")
+
 		} else {
 			fmt.Println(resp.Sid)
+			fmt.Println("Failed to send message")
 		}
 	}
 }
