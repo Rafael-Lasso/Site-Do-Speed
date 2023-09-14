41cdf119a59e0a036b6cc76603ff382f979302ee
Rafael Lasso <rafael.comercial27@gmail.com>
2023-09-13 23:40:48 -0300

    Organizing the Project architecture based on the principles of clean architecture

--- golang-notification/internal/domain/entity/notification.go
+++ golang-notification/internal/domain/entity/notification.go
@@ -7,33 +7,55 @@ import (
 )
 
 type Notification struct {
-	Id                 int
-	Title              string
-	Message            string
-	DateMsg            string
-	ActiveNotification bool
+	Id                 int               `json:"id"`
+	Title              string            `json:"title"`
+	Message            string            `json:"message"`
+	DateMsg            string            `json:"date"`
+	Topic              TopicNotification `json:"topic"`
+	ActiveNotification bool              `json:"active"`
 }
 
-func ValidNotification(n Notification) {
+type TopicNotification struct {
+	TopicId       int    `json:"id"`
+	TopicTitle    string `json:"title"`
+	TopicInterest bool   `json:"interest"`
+}
+
+func (t *TopicNotification) ValidTopicNotification() {
+
+	if t.TopicInterest == false {
+		log.Fatalln("Error! To send notification, user interest is required")
+	} else {
+		fmt.Println("Permission to send message validated successfully")
+	}
+
+}
+
+func (n Notification) ValidNotification() {
 
 	if n.ActiveNotification == false {
 		log.Fatalln("Error! To send notification, active permission is required")
 	} else {
 		fmt.Println("Permission to send message validated successfully")
-		fmt.Println(n.Message)
 	}
 
 }
 
 func main() {
 
-	note := Notification{
-		Id:                 1,
-		Title:              "Post novo",
-		Message:            "Acabou de sair um Post novo",
-		DateMsg:            "13-09-2023",
+	notific := Notification{
+		Id:      1,
+		Title:   "Post novo",
+		Message: "Acabou de sair um Post novo",
+		DateMsg: "13-09-2023",
+		Topic: TopicNotification{
+			TopicId:       1,
+			TopicTitle:    "Dragon Ball",
+			TopicInterest: true,
+		},
 		ActiveNotification: true,
 	}
 
-	ValidNotification(note)
+	notific.ValidNotification()
+	notific.Topic.ValidTopicNotification()
 }
