7dd6d4c93eed83fa423dd413e3e5862649c7f8a6
Rafael Lasso <rafael.comercial27@gmail.com>
2023-09-13 21:32:55 -0300

    Validation of permission to send notification message successful

--- golang-notification/internal/domain/entity/notification.go
+++ golang-notification/internal/domain/entity/notification.go
@@ -1,10 +1,39 @@
-package entity
+package main
 
-import "github.com/google/uuid"
+import (
+	"fmt"
+	"log"
+	// "github.com/google/uuid"
+)
 
 type Notification struct {
-	Id      uuid.UUID
-	Title   string
-	Message string
-	DateMsg string
+	Id                 int
+	Title              string
+	Message            string
+	DateMsg            string
+	ActiveNotification bool
+}
+
+func ValidNotification(n Notification) {
+
+	if n.ActiveNotification == false {
+		log.Fatalln("Error! To send notification, active permission is required")
+	} else {
+		fmt.Println("Permission to send message validated successfully")
+		fmt.Println(n.Message)
+	}
+
+}
+
+func main() {
+
+	note := Notification{
+		Id:                 1,
+		Title:              "Post novo",
+		Message:            "Acabou de sair um Post novo",
+		DateMsg:            "13-09-2023",
+		ActiveNotification: true,
+	}
+
+	ValidNotification(note)
 }
